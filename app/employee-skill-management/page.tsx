c9c90040fa5fc79ac64a7c33cf893533c7c2fe48
Vivek99256 <vivekgajera2709@gmail.com>
2025-04-02 10:10:37 +0530

    111

--- app/employee-skill-management/page.tsx
+++ app/employee-skill-management/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import { useRouter } from 'next/navigation';
 import JobIndustryTree from "../industry/[id]/JobIndustryTree";
 
 export default function EmployeeSkillPage() {
